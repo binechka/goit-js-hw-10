4e2ae60d6ea01977acfc900c506e38804d150ee3
Albina Bondar <albinabondar2019@gmail.com>
2022-08-22 00:10:53 +0300

    add

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,7 @@
 export const fetchCountries = (name) => {
-    name.preventDefault();
-    console.log(inputEl.value);
-    name = inputEl.value;
-return fetch(`https://restcountries.com/v3.1/name/${name}/?fields=name,capital,currencies,languages,flags`).then(r => r.json()).then(console.log)
+
+    return fetch(`https://restcountries.com/v3.1/name/${name}/?fields=name,capital,currencies,languages,flags&limit=10`).then(r => r.json()).then(data=>console.log(data))
+    
 }
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js'
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
-inputEl = document.querySelector("#search-box")
+const inputEl = document.querySelector("#search-box")
+const countryList = document.querySelector(".country-list")
+const countryinfo = document.querySelector(".country-info")
+
+function onSearch(e) {
+    e.preventDefault(); 
+    if (e.target.value.trim() !== "") {
+    fetchCountries(e.target.value.trim())
+}
+    }
 
 
+inputEl.addEventListener("input", debounce(onSearch,DEBOUNCE_DELAY))
 
+if (data.length > 10) {
+   return Notify.info("Too many matches found. Please enter a more specific name.");
+}
 
-inputEl.addEventListener("input", fetchCountries)
+if (2 < data.length < 10) {
+    return countryList.innerHTML = `<li>${flags} ${name}</li>`
+}
+
+if (data.length === 1) {
+    return countryinfo.innerHTML = `<h2>${flags} ${name}</h2>
+    <ul>
+    <li>Capital:${capital}</li>
+    <li>Population:${population}</li>
+    <li>Languages:${languages}</li>
+    </ul>
+    `
+}
