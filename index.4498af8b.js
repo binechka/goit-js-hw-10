b7b9d0ea40016a37201e4062ab97725b7f37fa4f
binechka <albinabondar2019@gmail.com>
2022-08-21 21:11:20 +0000

    Deploying to gh-pages from @ binechka/goit-js-hw-10@4e2ae60d6ea01977acfc900c506e38804d150ee3 🚀

--- index.4498af8b.js
+++
@@ -1,2 +0,0 @@
-inputEl=document.querySelector("#search-box"),inputEl.addEventListener("input",(function(e){return e.preventDefault(),console.log(inputEl.value),e=inputEl.value,fetch("https://restcountries.com/v3.1/name/".concat(e,"/?fields=name,capital,currencies,languages,flags")).then((function(e){return e.json()})).then(console.log)}));
-//# sourceMappingURL=index.4498af8b.js.map
