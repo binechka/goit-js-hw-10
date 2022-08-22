660815fa9abdbe32ddc90b69e26979c504e34a27
Albina Bondar <albinabondar2019@gmail.com>
2022-08-23 00:26:24 +0300

    add

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 export const fetchCountries = (name) => {
 
-    return fetch(`https://restcountries.com/v3.1/name/${name}/?fields=name,capital,currencies,languages,flags&limit=10`).then(r => r.json()).then(data=>console.log(data))
+    return fetch(`https://restcountries.com/v3.1/name/${name}/?fields=name,capital,currencies,population,languages,flags`).then(r => r.json()).then(data=>data)
     
 }
 
--- src/index.js
+++ src/index.js
@@ -5,32 +5,54 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector("#search-box")
 const countryList = document.querySelector(".country-list")
-const countryinfo = document.querySelector(".country-info")
+const countryInfo = document.querySelector(".country-info")
 
 function onSearch(e) {
     e.preventDefault(); 
     if (e.target.value.trim() !== "") {
-    fetchCountries(e.target.value.trim())
+        fetchCountries(e.target.value.trim()).then(data => {
+               
+            
+            if (data.length > 10) {
+   return Notify.info("Too many matches found. Please enter a more specific name.");
 }
+
+            if (data.length >= 2 && data.length <= 10) {
+                countryList.innerHTML = data.map(({name, flags}) => {
+                    return   `<li>
+  <img src="${flags.svg}" alt="${name.official}" width="30" height="25">
+  <p>${name.official}</p>
+</li>`
+    }).join("")
+   
+            }
+            else {
+               countryList.innerHTML = ""  
+            }
+
+if (data.length === 1) {
+    countryInfo.innerHTML = data.map(({ flags, name, capital, population, languages}) => {
+        return `<h2><img src="${flags.svg}" alt="${name.official}" width="30" height="25">
+       ${name.official}</h2>
+    <ul>
+    <li>Capital:${capital}</li>
+    <li>Population:${population}</li>
+    <li>Languages:${Object.values(languages)}</li>
+    </ul>
+    `
+    }).join("")
+    
+}
+            else {
+               countryInfo.innerHTML = ""  
+            }
+        }).catch((error) => {
+            Notify.failure('Qui timide rogat docet negare');
+        })
+        
+    }
     }
 
 
 inputEl.addEventListener("input", debounce(onSearch,DEBOUNCE_DELAY))
 
-if (data.length > 10) {
-   return Notify.info("Too many matches found. Please enter a more specific name.");
-}
-
-if (2 < data.length < 10) {
-    return countryList.innerHTML = `<li>${flags} ${name}</li>`
-}
-
-if (data.length === 1) {
-    return countryinfo.innerHTML = `<h2>${flags} ${name}</h2>
-    <ul>
-    <li>Capital:${capital}</li>
-    <li>Population:${population}</li>
-    <li>Languages:${languages}</li>
-    </ul>
-    `
-}
